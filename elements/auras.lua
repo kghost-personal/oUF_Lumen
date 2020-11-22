eb8e8a745ec620b37caa413105e447947cffde45
Zang MingJie <zealot0630@gmail.com>
2020-11-22 14:03:08 +0800

    Remote aura timer, and enlarge icon

--- elements/auras.lua
+++ elements/auras.lua
@@ -20,19 +20,11 @@ local function PostCreateIcon(self, button)
     count:SetFont(m.fonts.font, 12, "OUTLINE")
     count:SetPoint("TOPRIGHT", button, 3, 3)
 
-    button.icon:SetTexCoord(.08, .92, .08, .92)
+    button.icon:SetTexCoord(.02, .98, .02, .98)
 
     button.overlay:SetTexture(m.textures.aura_border)
     button.overlay:SetTexCoord(0, 1, 0, 1)
     button.overlay.Hide = function(self) self:SetVertexColor(0.1, 0.1, 0.1) end
-
-    button.time = button:CreateFontString(nil, "OVERLAY")
-    button.time:SetFont(m.fonts.font, 12, "THINOUTLINE")
-    button.time:SetPoint("BOTTOMLEFT", button, -2, -2)
-    button.time:SetTextColor(1, 1, 0.65)
-    button.time:SetShadowOffset(1, -1)
-    button.time:SetShadowColor(0, 0, 0, 1)
-    button.time:SetJustifyH("CENTER")
 
     -- For player debuffs show the spell name
     if unit == "player" and cfg.units[frame].auras.debuffs.spellName then
@@ -47,37 +39,11 @@ local function PostCreateIcon(self, button)
     end
 end
 
-local function OnUpdate(icon, elapsed)
-    if icon.timeLeft then
-        icon.timeLeft = max(icon.timeLeft - elapsed, 0)
-
-        -- Text color
-        if icon.timeLeft > 0 and icon.timeLeft < 60 then
-            icon.time:SetFormattedText(core:FormatTime(icon.timeLeft))
-            if icon.timeLeft < 6 then
-                icon.time:SetTextColor(0.9, 0.05, 0.05)
-            else
-                icon.time:SetTextColor(1, 1, 0.60)
-            end
-        else
-            icon.time:SetText()
-        end
-    end
-end
-
 local function PostUpdateIcon(icons, unit, icon, index)
     local name, _, _, _, duration, expirationTime =
         UnitAura(unit, index, icon.filter)
 
-    if duration and duration > 0 then
-        icon.timeLeft = expirationTime - GetTime()
-    else
-        icon.timeLeft = math.huge
-    end
-
     if (icon.spell) then icon.spell:SetText(name) end
-
-    icon:SetScript("OnUpdate", OnUpdate)
 end
 
 function lum:CreateAura(self, num, rows, size, spacing)
@@ -86,7 +52,6 @@ function lum:CreateAura(self, num, rows, size, spacing)
     auras.num = num
     auras.size = size
     auras.spacing = spacing or 6
-    auras.disableCooldown = true
     auras.PostCreateIcon = PostCreateIcon
     auras.PostUpdateIcon = PostUpdateIcon
     return auras
